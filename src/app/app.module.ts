c1fff3ba4c48de8f6dd28a9d78814a4fd0f2b31c
Chetan Bhat <chetanbhat999@gmail.com>
2021-02-23 12:31:48 +0000

    First commit
    Basic Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ANavbarComponent } from './a-navbar/a-navbar.component';
+import { BFooterComponent } from './b-footer/b-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ANavbarComponent,
+    BFooterComponent,
   ],
   imports: [
     BrowserModule,
